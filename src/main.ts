d7b795c56e006f08b1e0fc0f71703a5e9113a738
Ray <ll9907891@gmail.com>
2024-03-17 16:19:56 +0800

    專案初始化

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './_style.scss';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+import i18n from '@/assets/i18n';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(i18n);
+
+app.mount('#app');
